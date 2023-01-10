b524e39c64c98fdf02e2d193f38636136274b689
Samuel <spantoja0208@gmail.com>
2023-01-10 03:00:27 +0100

    comments added

--- NoGUI/main.py
+++ NoGUI/main.py
@@ -7,25 +7,32 @@ import final as fn
 import random as rnd
 import os
 
+#this function just creates the groups for the group fase
 def create_groups() -> dict:
     groups = {"group_A": grp.Group("A"), "group_B": grp.Group("B"), "group_C": grp.Group("C"), "group_D": grp.Group("D"), "group_E": grp.Group("E"), "group_F": grp.Group("F"), "group_G": grp.Group("G"), "group_H": grp.Group("H")}
     return groups
 
+#this assigns each team the user has given to a random group in an elegant way
+#gotta love recursive functions
 def get_random_group(groups) -> str:
     random_group = rnd.choice(list(groups.keys()))
+    #the return checks if the group has already 4 groups in it so every groups has that limit
     return random_group if len(groups.get(random_group).groupTeams()) < 4 else get_random_group(groups)
 
+#this functions acts as a medium taking the list of teams and the list of groups
 def assign_random_team(teams, groups) -> None:
     for team in teams:
         random_group = get_random_group(groups)
         groups.get(random_group).add_team(team)
 
+#this method gets the input from the user to create the 32 teams
 def add_teams_list(lst, groups) -> None:
     while len(lst) < 32:
         new_team = input("Introduce the next team: ")
         if new_team == "exit" or new_team == "EXIT" or new_team == "Exit":
             print("I'll save this for later ;)")
             break
+        #every new team is appended to a list
         elif new_team not in lst:
             new_country = tm.Team(new_team)
             lst.append(tm.Team(new_country))
@@ -36,9 +43,13 @@ def add_teams_list(lst, groups) -> None:
     if len(lst) == 32:
         assign_random_team(lst, groups)
 
+#this method generates the matches from the group phase
 def generate_matches(group) -> list:
+    #the matches list saves the different matches combinations per group, each one has 6
     matches = []
+    #used teams just makes sure a team doesn't play against itself and the matches don't repeat
     used_teams = []
+    #gets a list of teams per group
     teams = group.groupTeams()
     for team in teams.keys():
         used_teams.append(team)
@@ -49,18 +60,21 @@ def generate_matches(group) -> list:
                 continue
     return matches
 
+#this repeats the last method but for every group
 def matches_per_group(groups) -> dict:
     groups_matches = {}
     for group in groups:
         groups_matches[groups[group].groupName()] = generate_matches(groups[group])
     return groups_matches
 
-def select_group(group_name):
-    
+#this method is meant to show the user the matches per group so the user can select the match and give the score
+def select_group(groups_matches, group_name):
+    return groups_matches[group_name]
 
 def app() -> None:
     groups = create_groups()
     lst = []
+    group_fase_matches = {}
     while True:
         print("Choose wisely:")
         if not lst:
@@ -87,7 +101,8 @@ def app() -> None:
                 if select_2 == "1":
                     print(" Select a group: ")
                     group = input(" A, B, C, D, E, F, G or H")
-                    if group == "A":
+                    
+                    
 
         else:
             break
